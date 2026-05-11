57e7363cf89d88357c0e8f06efcc01032ce30fdd
Markyneos <mafaustine18@gmail.com>
2026-05-11 09:43:14 -0300

    Commit inicial pt 2

---
+++ backend/BackendApi/Models/Simulacao.cs
@@ -0,0 +1,14 @@
+﻿namespace BackendApi.Models
+{
+    public class Simulacao
+    {
+        public int Id { get; set; }
+        public decimal ValorInicial { get; set; }
+        public int Dias { get; set; }
+        public decimal TaxaAnual { get; set; }
+        public decimal RendimentoBruto { get; set; }
+        public decimal Imposto { get; set; }
+        public decimal Liquido { get; set; }
+        public DateTime DataConsulta { get; set; } = DateTime.Now;
+    }
+}
